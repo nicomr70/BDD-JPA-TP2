6a3cece324c52cb0aca521c85c03fd5fe825f9ae
tranthiv <thi-van-anh.tran@etu.univ-grenoble-alpes.fr>
2024-03-14 16:53:00 +0100

    correction

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/ClubSportifEntity.java
@@ -0,0 +1,19 @@
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "ClubSportif")
+public class ClubSportifEntity {
+    @Id
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nom")
+    private String nom;
+    @Column(name = "description")
+    private String description;
+    @ManyToMany
+    private Set<EtudiantEntity> etudiants;
+
+}
---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/ProjetEntity.java
@@ -0,0 +1,20 @@
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.*;
+import java.math.BigInteger;
+import java.util.Set;
+
+@Entity
+@Table(name = "projet")
+public class ProjetEntity {
+    @Id
+    @Column(name = "id")
+    private BigInteger id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @ManyToMany
+    private Set<DeveloperMiageEntity> developers;
+
+}
