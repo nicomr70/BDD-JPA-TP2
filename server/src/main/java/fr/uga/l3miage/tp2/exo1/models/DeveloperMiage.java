306e6e58f22be1d0a4dd8ce7407b042338d862a6
l3miage-karmouah <ahmed.karmous.etu@gmail.com>
2024-03-17 21:29:45 +0100

    fin de l'exo1

--- server/src/main/java/fr/uga/l3miage/tp2/exo1/models/DeveloperMiage.java
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/DeveloperMiage.java
@@ -1,2 +1,21 @@
-package fr.uga.l3miage.tp2.exo1.models;public class developerMiage {
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class DeveloperMiage {
+
+    @Id
+    private String lastname;
+
+    private String firstname;
+
+    private String email;
+
+    @ManyToMany(mappedBy = "developers")
+    private Set<Projet> Projets;
+
 }
--- server/src/main/java/fr/uga/l3miage/tp2/exo1/models/Etudiant.java
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/Etudiant.java
@@ -1,2 +1,19 @@
-package fr.uga.l3miage.tp2.exo1.models;public class Etudiant {
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class Etudiant {
+    @Id
+    private Long agalan;
+
+    private String nom;
+
+    private String email;
+
+    @ManyToMany(mappedBy = "etudiants")
+    private Set<ClubSportif> clubs;
 }
