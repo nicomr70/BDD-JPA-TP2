97c1a5712a144d053e75b59d19b3185538c14045
morabitb <basma.morabit@etu.univ-grenoble-alpes.fr>
2024-03-21 15:48:04 +0100

    fin exo1/TP2

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo1/models/DeveloperMiageEntity.java
@@ -0,0 +1,19 @@
+package fr.uga.l3miage.tp2.exo1.models;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "developer_miage")
+public class DeveloperMiageEntity {
+    @Id
+    @Column(name = "lastname")
+    private String lastname ;
+    @Column(name = "firstname")
+    private String firstname ;
+    @Column(name = "email")
+    private String email ;
+    @ManyToMany(mappedBy = "developers")
+    private Set <ProjetEntity> projets ;
+
+}
