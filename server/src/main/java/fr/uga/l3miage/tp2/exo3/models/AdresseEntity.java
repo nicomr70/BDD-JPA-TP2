b88b89eed89bb40c5ea6519f75936c2ee1dd90a0
morabitb <basma.morabit@etu.univ-grenoble-alpes.fr>
2024-03-22 00:04:09 +0100

    fin d'exo3/TP2

---
+++ server/src/main/java/fr/uga/l3miage/tp2/exo3/models/AdresseEntity.java
@@ -0,0 +1,17 @@
+package fr.uga.l3miage.tp2.exo3.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class AdresseEntity {
+    @Id
+    private Long id ;
+    private String rue ;
+    private String numero ;
+    private String codePostal ;
+    private String ville ;
+    @OneToOne
+    private ClientEntity client ;
+}
